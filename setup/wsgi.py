cf5b234fe93ea56b2ba28da04f5e8deb1d179920
davissontiago <davissontiagolf@gmail.com>
2025-12-26 19:28:19 -0300

    att2

--- setup/wsgi.py
+++ setup/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for setup project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +5,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
 
 application = get_wsgi_application()
+
+# ADICIONE ESTA LINHA PRO VERCEL:
+app = application
