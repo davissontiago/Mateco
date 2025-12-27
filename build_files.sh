c6d04fbb3c4e131b6aec8a26a33c93f1643ed5e1
davissontiago <davissontiagolf@gmail.com>
2025-12-27 20:53:40 -0300

    Testando criar a pasta public

--- build_files.sh
+++ build_files.sh
@@ -2,3 +2,6 @@
 echo "Building the project..."
 pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
+
+# Cria uma pasta vazia apenas para o Vercel não dar erro
+mkdir -p public
