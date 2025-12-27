c04b89dd5cf94369659877e0e890a41d97c74066
davissontiago <davissontiagolf@gmail.com>
2025-12-26 23:04:19 -0300

    corrigido static

--- setup/settings.py
+++ setup/settings.py
@@ -84,7 +84,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'pt-br'
 TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
-USE_TZ = True
+USE_TZ = True 
 
 # --- ARQUIVOS ESTÁTICOS (CSS, JS, IMAGENS) ---
 STATIC_URL = 'static/'
