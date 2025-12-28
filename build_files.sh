4a65e0f7aa84d94072473d53a50291a5e165664b
davissontiago <davissontiagolf@gmail.com>
2025-12-27 21:01:15 -0300

    Sem gambiarra não deu

--- build_files.sh
+++ build_files.sh
@@ -2,3 +2,8 @@
 echo "Building the project..."
 pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
+
+# Cria a pasta public
+mkdir -p public
+# Cria um arquivo simples dentro dela para o Vercel validar o build
+echo "Build Sucesso" > public/index.html
